faa3dddf1c5b2f353e4907a18c05b7f00822ef8a
deepakmanghani1511 <deepakmanghani2002@gmail.com>
2025-04-05 16:10:47 +0530

    First commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,62 @@
+pipeline {
+    agent any
+
+    environment {
+        AZURE_CREDENTIALS_ID = 'azure-service-principal-3'
+        RESOURCE_GROUP = 'rg-jenkins-123'
+        APP_SERVICE_NAME = 'deep-integrated-dot-net' // must be unique
+    }
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: ''
+            }
+        }
+
+        stage('Build') {
+            steps {
+                bat 'dotnet restore' // it install all nuget packages which are present in dependicies packages in vs 
+                bat 'dotnet build --configuration Release'
+                bat 'dotnet publish -c Release -o ./publish' // it create a two folder bin(final output) and obj(temporary) 
+            }
+        }
+
+        stage('Test Terraform') {
+            steps {
+                dir('terraform') {
+                    bat 'terraform --version'
+                }
+            }
+        }
+
+        stage("terraform setup") {
+            steps {
+                dir("terraform") {
+                    bat 'terraform init'
+                    bat 'terraform plan -out=tfplan'
+                    bat 'terraform apply -auto-approve tfplan'
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                withCredentials([azureServicePrincipal(credentialsId: AZURE_CREDENTIALS_ID)]) {
+                    bat "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID"
+                    bat "powershell Compress-Archive -Path ./publish/* -DestinationPath ./publish.zip -Force"
+                    bat "az webapp deploy --resource-group $RESOURCE_GROUP --name $APP_SERVICE_NAME --src-path ./publish.zip --type zip"
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Deployment Successful!'
+        }
+        failure {
+            echo 'Deployment Failed!'
+        }
+    }
+}
